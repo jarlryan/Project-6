5759a0cb6aa9e7c08016f7626d0ddc64421be995
Ryan Jarl <91164625+jarlryan@users.noreply.github.com>
2021-09-23 11:06:53 -0400

    Update StringsAndEnumerations.cpp

--- StringsAndEnumerations.cpp
+++ StringsAndEnumerations.cpp
@@ -4,7 +4,7 @@
    that contains data that consists of several lines,
    each with one person and information about them
    regarding their gender, and marital status, along
-   with their names in a misorganized fashion. The program
+   with their names in an unorganized fashion. The program
    utilizes strings and enumerations to decide their 
    standardized name and prefixes in an organized style,
    written to an output file (mp6output.txt).*/
